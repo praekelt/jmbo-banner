0bcfcc41066773d4992be137adea2359f761edcd
Phala Ramonyai <phalamag@gmail.com>
2017-11-26 20:12:45 +0200

    Add tests for views

--- banner/tests/urls.py
+++ banner/tests/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
-    url(r'^jmbo/', include('jmbo.urls')),
-    url(r'^comments/', include('django_comments.urls')),
-    # (r'^banner/', include('banner.urls')),
+    url(r"^jmbo/", include("jmbo.urls")),
+    url(r"^comments/", include("django_comments.urls")),
+    url(r'^banner/', include("banner.urls", namespace="banner")),
 ]
--- banner/urls.py
+++ banner/urls.py
@@ -1,4 +1,17 @@
 from django.conf.urls import url
 
+from banner.views import BannerDetailView, BannerListView
 
-urlpatterns = []
+
+urlpatterns = [
+    url(
+        r"^detail/(?P<slug>[\w-]+)/$",
+        BannerDetailView.as_view(),
+        name="banner-detail"
+    ),
+    url(
+        r"^$",
+        BannerListView.as_view(),
+        name="banner-list"
+    ),
+]
