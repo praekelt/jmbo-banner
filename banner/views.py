2e91c1434ca26f58f39ea4acba486637168f37fe
Phala Ramonyai <phalamag@gmail.com>
2017-11-26 19:43:39 +0200

    Add detail and list views

--- banner/views.py
+++ banner/views.py
@@ -1,1 +1,18 @@
+from jmbo.views import ObjectDetail, ObjectList
 
+from banner.models import Banner
+
+
+class BannerDetailView(ObjectDetail):
+    model = Banner
+    template_name = "banner/banner_detail.html"
+
+    def get_template_names(self):
+        template_names = [self.template_name]
+        other_names = super(BannerDetailView, self).get_template_names()
+        template_names.extend(other_names)
+
+
+class BannerListView(ObjectList):
+    model = Banner
+    template_name = "banner/banner_list.html"
