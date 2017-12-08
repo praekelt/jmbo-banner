f7096c05a39f14fa8fcd2d9d5db14e07b099edf5
Phala Ramonyai <phalamag@gmail.com>
2017-12-08 13:07:05 +0200

    Temporarily limit to Django 1.11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='http://github.com/praekelt/jmbo-banner',
     packages=find_packages(),
     install_requires=[
-        'django>=1.11',
+        'django>=1.11,<2.0',
         'django-link',
         'django-simplemde',
         'jmbo',
