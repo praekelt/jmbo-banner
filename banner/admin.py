ea726891796d3058fa607efed421793ef193e318
Phala Ramonyai <phalamag@gmail.com>
2017-12-11 00:21:00 +0200

    Modify admin

--- banner/admin.py
+++ banner/admin.py
@@ -5,8 +5,14 @@ from jmbo.admin import ImageInline, ModelBaseAdmin
 from banner.models import Banner, Button
 
 
+class ButtonAdminInline(admin.TabularInline):
+    model = Banner.buttons.through
+    verbose_name = "Button"
+    verbose_name_plural = "Buttons"
+
+
 class BannerAdmin(ModelBaseAdmin):
-    inlines = [ImageInline, ]
+    inlines = [ImageInline, ButtonAdminInline]
 
 
 admin.site.register(Banner, BannerAdmin)
